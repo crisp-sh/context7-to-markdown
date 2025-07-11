3de272edffdf7f4e462547385595be48db12e1fd
sellerscrisp <sellershcrisp@gmail.com>
2025-07-11 15:11:31 +0300

    chore: bump version to 0.2.0

--- c2md/__init__.py
+++ c2md/__init__.py
@@ -2,6 +2,6 @@
 c2md - CLI tool to convert Context7 llms.txt format to locally organized markdown documentation.
 """
 
-__version__ = "0.1.0"
-__author__ = "crisp.sh"
+__version__ = "0.2.0"
+__author__ = "crisp-sh"
 __email__ = "s@crisp.sh"
